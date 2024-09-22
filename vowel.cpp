eb0f27fd906d953e05b79cccd72f96ba4c55fa9b
Sanjeet Kumar <sanjeetkum960@gmail.com>
2024-09-22 15:18:41 +0530

    week

---
+++ vowel.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char C;
+    cin>> C;
+    if(C='a')
+    cout<<"vowel";
+    
+    else if(C='e')
+    cout<<"vowel";
+
+    else if(C='i')
+    cout<<"vowel";
+    else if(C='o')
+    cout<<"vowel";
+    else if(C='u')
+    cout<<"vowel";
+    else
+     cout<<"consonent";
+    
+}
